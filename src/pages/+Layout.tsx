cd55f7237ce581b882d1de31f009a5440ebdd679
Tanyawat Vittaypalaotai <hamzaabamboo@gmail.com>
2024-06-15 05:05:05 +0900

    fix: lint

--- src/pages/+Layout.tsx
+++ src/pages/+Layout.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { ToasterProvider } from '~/context/ToasterContext';
-import '../index.css';
-import ErrorBoundary from '~/components/utils/ErrorBoundary';
-
-import '../i18n';
 import { HelmetProvider } from 'react-helmet-async';
+import ErrorBoundary from '~/components/utils/ErrorBoundary';
+import { ToasterProvider } from '~/context/ToasterContext';
+import '../i18n';
+import '../index.css';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
