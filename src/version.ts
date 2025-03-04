7229b5f94945f146b5b3ce6283bcefc891db988e
Tanyawat Vittaypalotai <vittayapalotai.tanyawat@arsaga.jp>
2025-03-04 17:24:46 +0900

    chore: release v1.1.3

--- src/version.ts
+++ src/version.ts
@@ -1,15 +1,15 @@
 // This file is auto-generated. Do not edit manually.
-// Generated on: 2025-03-04T08:17:15.517Z
+// Generated on: 2025-03-04T08:24:37.166Z
 
 /**
  * Application version from package.json
  */
-export const VERSION = '1.1.2';
+export const VERSION = '1.1.3';
 
 /**
  * Build timestamp
  */
-export const BUILD_TIMESTAMP = '2025-03-04T08:17:15.517Z';
+export const BUILD_TIMESTAMP = '2025-03-04T08:24:37.166Z';
 
 /**
  * Returns the application version with build information
