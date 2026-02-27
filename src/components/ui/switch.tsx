c41877d4039cbd24f7c79111ed35f86c86ba8c72
Michael Ting <michaelting@meta.com>
2026-02-26 18:14:33 -0800

    fix: add visible bounding box and allow clicks when focused for switches and checkboxes

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react';
+import { forwardRef, type KeyboardEvent } from 'react';
 import * as StyledSwitch from './styled/switch';
 
 export type SwitchProps = StyledSwitch.RootProps;
@@ -8,11 +8,23 @@ export const Switch = forwardRef<HTMLLabelElement, SwitchProps>((props, ref) => 
 
   return (
     <StyledSwitch.Root ref={ref} {...rootProps}>
-      <StyledSwitch.Control>
+      <StyledSwitch.Control
+        _focusVisible={{
+          outlineOffset: '2px',
+          outline: '2px solid',
+          outlineColor: 'border.outline'
+        }}
+      >
         <StyledSwitch.Thumb />
       </StyledSwitch.Control>
       {children && <StyledSwitch.Label>{children}</StyledSwitch.Label>}
-      <StyledSwitch.HiddenInput />
+      <StyledSwitch.HiddenInput
+        onKeyDown={(e: KeyboardEvent<HTMLInputElement>) => {
+          if (e.key === 'Enter') {
+            e.currentTarget.click();
+          }
+        }}
+      />
     </StyledSwitch.Root>
   );
 });
